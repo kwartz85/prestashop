96638b04f2ae590751c25c45de5091724f0a744f
francois masson <masson.francois@gmail.com>
2017-02-22 09:15:05 +0100

    envoi du 170222

--- controllers/front/MyController.php
+++ controllers/front/MyController.php
@@ -12,7 +12,17 @@ if (!defined('_PS_VERSION_')) {
 
 class CustomerCommentMyControllerModuleFrontController extends FrontController
 {
-   public function initContent(){
+   public function initContent()
+   {
+       parent::initContent();
+       $action = Tools::getValue('action');
+
+       if (!Tools::isSubmit('myajax'))
+       $this->assign();
+       elseif (!empty($action) && method_exists($this, 'ajaxProcess'.Tools::toCamelCase($action)))
+       $this->{'ajaxProcess'.Tools::toCamelCase($action)}();
+       else
+       die(Tools::jsonEncode(array('error' => 'method doesn\'t exist')));
 
    }
 
